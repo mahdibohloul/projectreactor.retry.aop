7b6551625dd1b2deb11752c8611c487f29bf1038
Mahdi Bohloul <mehdi.bohlol@tapsi.net>
2022-07-10 19:08:21 +0430

    Initial project

---
+++ src/test/java/io/github/mahdibohloul/projectreactor/retry/aop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mahdibohloul.projectreactor.retry.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
