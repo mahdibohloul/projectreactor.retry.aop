dd6eb3447d7cece66daf70bca54174ab0e50e4c7
Mahdi Bohloul <mehdi.bohlol@tapsi.net>
2022-07-30 12:31:46 +0430

    Fix violence

--- src/main/java/io/github/mahdibohloul/projectreactor/retry/aop/annotation/EnableReactiveRetry.java
+++ src/main/java/io/github/mahdibohloul/projectreactor/retry/aop/annotation/EnableReactiveRetry.java
@@ -1,14 +1,14 @@
 package io.github.mahdibohloul.projectreactor.retry.aop.annotation;
-;
+
 import java.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 
 /**
- * Enable reactive retryable aop capability. To be used on {@link
- * org.springframework.context.annotation.Configuration @Configuration} classes to configure
- * reactive retryable aop.
+ * Enable reactive retryable aop capability. To be used on
+ * {@link org.springframework.context.annotation.Configuration @Configuration}
+ * classes to configure reactive retryable aop.
  *
  * @author mahdibohloul
  * @version 1.0
@@ -19,13 +19,13 @@ import org.springframework.core.Ordered;
 @Import(ReactiveRetryConfiguration.class)
 @Documented
 public @interface EnableReactiveRetry {
-  /**
-   * The order of the reactive retryable aop. The default value is {@link
-   * Ordered#LOWEST_PRECEDENCE}.
-   *
-   * @return The order of the reactive retryable aop.
-   */
-  int order() default Ordered.LOWEST_PRECEDENCE;
+    /**
+     * The order of the reactive retryable aop. The default value is
+     * {@link Ordered#LOWEST_PRECEDENCE}.
+     *
+     * @return The order of the reactive retryable aop.
+     */
+    int order() default Ordered.LOWEST_PRECEDENCE;
 
-  boolean proxyTargetClass() default false;
+    boolean proxyTargetClass() default false;
 }
